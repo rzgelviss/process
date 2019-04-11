1aa9ceee9d5c6a486ed8a6263e9a47bac55e1708
user1 <user1.126.com>
2019-04-11 11:08:04 +0800

    dfs

--- preprocess.py
+++ preprocess.py
@@ -41,6 +41,45 @@ def outliers(df):
     li_col = [col for col in df]
     for col in li_col:
         label = df[col].value_counts(dropna=False).index.tolist()
+        for i ,num in enumerate(df[col].value_counts(dropna=False).values):
+            if num <= outliers_threshold:
+                df.loc[df[col] == label[i], [col]] = np.NaN
+    return df
+
+#单一类别特征占比大于threshold特征删除
+def single_feature(df):
+    single_threshold = 0.98
+    li_col = [col for col in df]
+    for col in li_col:
+        rate = df[col].value_counts(normalize=True, dropna=False).values[0]
+        if rate > single_threshold:
+            print('类别{0}占比{1}'.format(col,rate))
+            df.drop(col, axis=1, inplace=True)
+    return df
+
+#特征占比图
+def show_feature_rate(df,col):
+    cnt = df[col].value_counts(dropna=False).sort_index(ascending=True)
+    plt.figure(figsize=(10, 5))
+    cnt.plot.bar()
+    plt.title(col)
+    plt.show()
+
+#共线特征删除
+def correlation(df):
+    li_col = [col for col in df.columns]
+    corr_matrix = df.corr()
+    correlation_threshold = 0.98
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+    record_collinear = pd.DataFrame(columns=['drop_feature', 'corr_feature', 'corr_value'])
+    for column in to_drop:
+        corr_features = list(upper.index[upper[column].abs() > correlation_threshold])
+        col_values = list(upper[column][upper[]])
+
+
+
+
 
 
 
